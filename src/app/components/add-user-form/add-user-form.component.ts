1ce3bf535014edb9e9a2ff2af670dc835f458927
Adriano Cassio <adricarr@gmail.com>
2020-09-26 20:01:08 -0300

    Adicionando amigo

--- src/app/components/add-user-form/add-user-form.component.ts
+++ src/app/components/add-user-form/add-user-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserTableComponent } from '../user-table/user-table.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FriendService } from 'src/app/shared/services/friend.service'
 
 @Component({
   selector: 'add-user-form',
@@ -9,20 +11,16 @@ import { UserTableComponent } from '../user-table/user-table.component';
 })
 export class AddUserFormComponent implements OnInit {
 
-  /** Ao adicionar vc vai retornar um evento, para atualizar a tabela. */
   @Output() onAdd: EventEmitter<any> = new EventEmitter<any>();
-  // Recebe a tabela de usuário para manipular ela.
   @Input() table: UserTableComponent;
   submitted: boolean = false;
 
   public loginForm: FormGroup = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.min(5)]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', Validators.required),
+    phone: new FormControl('', [Validators.required]),
   });
 
-  /** importar o service e o snakebar para exibir mensagnes */
-  constructor() { }
+  constructor(private snackBar: MatSnackBar, private service: FriendService) { }
 
   ngOnInit(): void {
   }
@@ -30,25 +28,29 @@ export class AddUserFormComponent implements OnInit {
   get f() { return this.loginForm }
 
   onSubmit(){
-    // if (this.f.invalid) {
-    //   this.snackBar.open('Verifique os campos preenchidos', 'OK', {
-    //     duration: 2000,
-    //   });
-    //   return;
-    // }
+    if (this.f.invalid) {
+      this.snackBar.open('Verifique os campos preenchidos', 'OK', {
+        duration: 2000,
+      });
+      return;
+    }
 
-    // this.submitted = true;
+    this.submitted = true;
 
-    // this.service.salvarUsuário(this.f.value).then((resp) => {
-    //  ****** Se salvar com sucesso *****
-     this.onAdd.emit({success: true})
-     this.table.load(); // -> vai retornar um evento
-    // }).catch(() => { 
-    //   this.snackBar.open('Falha ao adicionar', 'OK', {
-    //     duration: 2000,
-    //   });
-    //   this.submitted = false; 
-    // })
+    this.service.save(this.f.value).then((resp) => {
+     //this.onAdd.emit({success: true})
+      this.f.reset();
+      this.table.load();
+      this.snackBar.open('Sucesso ao Adiconar', 'OK', {
+        duration: 2000,
+      });
+      this.submitted = false; 
+    }).catch(() => { 
+      this.snackBar.open('Falha ao adicionar', 'OK', {
+        duration: 2000,
+      });
+      this.submitted = false; 
+    })
   }
 
 }
